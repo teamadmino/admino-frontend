4bb7c489e84a7470f7f06b354659791b66a03b6a
teamadmino <teamadmino@icloud.com>
2020-07-28 13:12:59 -0700

    progressbar code cleanup and improvements

--- projects/admino/src/lib/modules/admino-screen/elements/progressbar/progressbar.component.ts
+++ projects/admino/src/lib/modules/admino-screen/elements/progressbar/progressbar.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { AdminoScreenElement } from "../admino-screen-element";
-import { v4 as uuidv4 } from "uuid";
 import { ScreenElementChange } from "../../admino-screen.interfaces";
-import { has } from "lodash";
+import { deepMerge } from "../../../../utils/deepmerge";
 
 @Component({
   selector: "admino-progressbar",
@@ -10,178 +9,71 @@ import { has } from "lodash";
   styleUrls: ["./progressbar.component.scss"],
 })
 export class ProgressBarComponent extends AdminoScreenElement implements OnInit {
-  // idPrefix;
-
   @ViewChild("innerRef", { static: true }) innerRef: ElementRef;
   @ViewChild("wrapperRef", { static: true }) wrapperRef: ElementRef;
-  defaultSetup = {
+
+  setup = {
     percent: 0,
-    width: 600,
-    height: 50,
-    animation: "on",
+    showValue: true,
+    animation: true,
+    preloaderColor: "#040",
+    wrapperColor: "#444",
+    textColor: "#FFF",
+    textHighlight: "#AA0",
+    prefix: " Running ",
+    postfix: "%",
+    staticLabel: "",
   };
 
-  frequency = 20;
-  colorFrequency = 100;
-  setup: any = {};
-  timerInterval;
-  divFull;
-  divCompleted;
+  private = {
+    displayValue: undefined,
+    appliedColor: undefined,
+    animationFrame: undefined,
+    restoreTextColorTimeout: undefined,
+  };
 
-  percent = 20;
-  animation = "on";
-  preloaderColor = "#220000";
-  wrapperColor = "#444444";
-  textColor = "#FFFFFF";
-  textHighlight = "#0000FF";
-
-  appliedColor = this.textColor;
-
-  updated = false;
-  applyAnimation = true;
-  label = 0;
-  anim;
-  timeout;
   ngOnInit() {
-    // this.idPrefix = "admino_" + this.element.id + "_" + uuidv4() + "_";
-    // this.cd.detectChanges();
-    // this.onResize();
-    // if (this.element.setup) {
-    //   Object.assign(this.setup, this.element.setup);
-    // }
-    // this.setupProgressBar();
-    // setInterval((params) => {
-    //   this.getLabel();
-    //   this.directive.cd.markForCheck();
-    //   // console.log(this.innerRef.nativeElement.clientWidth);
-    // }, 10);
-    this.percent = this.element.setup.percent;
-    this.animate();
+    this.setup = deepMerge(this.setup, this.element.setup);
+    this.private.appliedColor = this.setup.textColor;
+    this.updateLabel();
   }
 
-  animate() {
-    this.anim = window.requestAnimationFrame((params) => {
-      console.log("animating");
-      if (Math.abs(this.innerRef.nativeElement.clientWidth - (this.percent / 100) * this.wrapperRef.nativeElement.clientWidth) > 2) {
-        this.label = Math.floor((this.innerRef.nativeElement.clientWidth / this.wrapperRef.nativeElement.clientWidth) * 100);
-        this.animate();
+  onChange(changes: { [id: string]: ScreenElementChange }) {
+    Object.assign(this.setup, this.element.setup);
+    this.highlightText();
+    this.updateLabel();
+  }
+
+  onDestroy() {
+    cancelAnimationFrame(this.private.animationFrame);
+    clearTimeout(this.private.animationFrame);
+    clearTimeout(this.private.restoreTextColorTimeout);
+  }
+
+  onResize() {}
+
+  updateLabel() {
+    this.private.animationFrame = window.requestAnimationFrame(() => {
+      if (
+        this.setup.animation &&
+        Math.abs(this.innerRef.nativeElement.clientWidth - (this.setup.percent / 100) * this.wrapperRef.nativeElement.clientWidth) > 2
+      ) {
+        this.private.displayValue = Math.floor((this.innerRef.nativeElement.clientWidth / this.wrapperRef.nativeElement.clientWidth) * 100);
+        this.updateLabel();
       } else {
-        this.label = this.percent;
+        this.private.displayValue = this.setup.percent;
       }
       this.directive.cd.markForCheck();
     });
   }
 
-  setupProgressBar() {
-    // this.setupParameter("height");
-    // this.setupParameter("width");
-    // this.setupParameter("percent");
-    // this.setupParameter("animation");
-    // this.setup.current = 0;
-    // this.divFull = document.getElementById(this.idPrefix + "progressbardiv");
-    // this.divFull.style.height = this.setup.height + "px";
-    // this.divFull.style.lineHeight = this.setup.height + "px";
-    // this.divFull.style.width = this.setup.width + "px";
-    // this.divFull.style.position = "relative";
-    // this.divFull.style.backgroundColor = "#777";
-    // this.divFull.style.color = "#FFF";
-    // this.divFull.innerHTML = "<div id='" + this.idPrefix + "inner'></div>";
-    // this.divCompleted = document.getElementById(this.idPrefix + "inner");
-    // this.divCompleted.style.height = this.setup.height + "px";
-    // this.divCompleted.style.width = "0px";
-    // this.divCompleted.style.backgroundColor = "#060";
-    // this.divCompleted.style.textAlign = "center";
-    // this.divCompleted.innerHTML = "&nbspRunning&nbsp";
-    // this.setup.updated = 0;
-    // this.updateDiv();
-  }
-
-  // setupParameter(param) {
-  //   if (this.element.setup != undefined) {
-  //     if (this.element.setup[param] != undefined) {
-  //       this.setup[param] = this.element.setup[param];
-  //       this.element.setup[param] = undefined;
-  //     }
-  //   }
-  //   if (this.setup[param] === undefined) {
-  //     this.setup[param] = this.defaultSetup[param];
-  //   }
-  // }
-
-  onChange(changes: { [id: string]: ScreenElementChange }) {
-    if (changes.setup) {
-      // Object.assign(this.setup, this.element.setup);
-      // this.wrapperColor = has(this.element.setup,'wrapperColor.color.valami')? this.element.setup.wrapperColor;
-      // this.applyAnimation = false;
-      // this.directive.cd.detectChanges();
-      // this.appliedColor = this.textHighlight;
-      // this.directive.cd.detectChanges();
-      // setTimeout((params) => {
-      //   this.applyAnimation = true;
-      //   this.directive.cd.detectChanges();
-      // }, 10);
-      // setTimeout((params) => {
-      //   this.appliedColor = this.textColor;
-      //   this.percent = this.element.setup.percent;
-      //   this.directive.cd.detectChanges();
-      // }, 100);
-      this.percent = this.element.setup.percent;
-      this.highlightText();
-
-      this.animate();
-
-      // this.animation = this.element.setup.animation;
-      // this.preloaderColor = this.element.setup.preloaderColor ? this.element.setup.preloaderColor : "0xfff0000";
-      // this.wrapperColor = this.element.setup.wrapperColor ? this.element.setup.wrapperColor : "0x44444";
-
-      // this.highlighText();
-    }
-    // if (changes._forceRefresh) {
-    //   console.log("refresh");
-    // }
-    // console.log(changes);
-    // this.setupParameter("percent");
-    // this.setupParameter("animation");
-    // this.setup.updated = 16;
-    // this.updateDiv();
-  }
   highlightText() {
-    this.appliedColor = this.textHighlight;
-    this.timeout = setTimeout(() => {
-      this.appliedColor = this.textColor;
-    }, 1000);
+    if (this.setup.animation) {
+      this.private.appliedColor = this.setup.textHighlight;
+      this.private.restoreTextColorTimeout = setTimeout(() => {
+        this.private.appliedColor = this.setup.textColor;
+        this.directive.cd.markForCheck();
+      }, 500);
+    }
   }
-
-  onDestroy() {
-    cancelAnimationFrame(this.anim);
-    clearInterval(this.timerInterval);
-    clearTimeout(this.anim);
-  }
-
-  onResize() {}
-
-  // updateDiv() {
-  //   clearInterval(this.timerInterval);
-  //   var div2 = document.getElementById(this.idPrefix + "inner");
-  //   if (this.setup.animation != "on") {
-  //     this.setup.current = this.setup.percent;
-  //     this.setup.updated = 0;
-  //   } else {
-  //     var diff = this.setup.percent - this.setup.current;
-  //     var step = (((Math.abs(diff) + 19) / 20) | 0) * Math.sign(diff);
-  //     this.setup.current += step;
-  //     if (this.setup.updated > 0) this.setup.updated--;
-  //   }
-  //   var color = "#" + (0xfff - this.setup.updated).toString(16);
-  //   div2.style.width = (this.setup.width * this.setup.current) / 100 + "px";
-  //   div2.innerHTML = "&nbspRunning&nbsp" + this.setup.current + "%";
-  //   div2.style.color = color;
-  //   if (this.setup.percent == this.setup.current && this.setup.updated == 0) return;
-  //   this.timerInterval = setInterval(
-  //     () => {
-  //       this.updateDiv();
-  //     },
-  //     this.setup.percent == this.setup.current ? this.colorFrequency : this.frequency
-  //   );
-  // }
 }
